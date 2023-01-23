6c611b6475ce485c2c976dae02bee65236c0c8be
Christian Carollo <carollochristian6@gmail.com>
2023-01-23 18:42:29 +0100

    Initial commit

---
+++ src/main/java/co/develhope/firstapi4/controllers/HeaderController.java
@@ -0,0 +1,17 @@
+package co.develhope.firstapi4.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/headers")
+public class HeaderController {
+
+    @GetMapping
+    public String getHeaders(@RequestHeader("host") String host) {
+        return "Host: " + host;
+    }
+}
+
