d3c67f8a8013ba665ca2a0eb8416898ecc081bbd
Christian Carollo <carollochristian6@gmail.com>
2023-02-10 15:18:18 +0100

    modifica

--- src/main/java/co/develhope/firstapi4/controllers/HeaderController.java
+++ src/main/java/co/develhope/firstapi4/controllers/HeaderController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/headers")
 public class HeaderController {
 
-    @GetMapping("/get")
+    @GetMapping("/headers")
     public String getHeaders(@RequestHeader("host") String host) {
         return "Host: " + host;
     }
